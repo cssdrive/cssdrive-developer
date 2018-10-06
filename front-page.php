c1d002759c85747746ab275376866ad19f7ea4e6
Fubon <thefubon@gmail.com>
2018-10-06 21:42:27 +0300

    Добавлен файл "Главной страницы"

---
+++ front-page.php
@@ -0,0 +1,36 @@
+<?php get_header(); ?>
+
+	<!--  Main Content
+  ============================================================ -->
+  
+	<main id="main" role="main">
+		
+		<h2 class="ukheading">Слайдер постов</h2>
+    <div id="sync1" class="owl-carousel preloading">
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538847939075" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538847958924" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848035961" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848045832" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848053319" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848060702" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848069390" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848078661" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848086694" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848094863" width="100%"></div>
+    </div>
+    <div id="sync2" class="owl-carousel uk-margin">
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538847939075" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538847958924" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848035961" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848045832" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848053319" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848060702" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848069390" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848078661" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848086694" width="100%"></div>
+      <div class="item"><img src="https://placeimg.com/960/380/tech?t=1538848094863" width="100%"></div>
+    </div>
+		
+	</main><!-- #main -->
+
+<?php get_footer(); ?>
