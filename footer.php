84f544e669815843bc1b50ec646ed059352d0763
TheFubon <43171309+cssdrive@users.noreply.github.com>
2019-03-19 18:06:04 +0300

    Update
    Date

--- footer.php
+++ footer.php
@@ -7,7 +7,7 @@
 	<div class="uk-section uk-section-xsmall uk-section-muted">
 		<div class="uk-container uk-text-center uk-text-meta">
 		  
-		  &copy&nbsp;2015-<?php echo date( 'Y' ) ?> <strong><?php echo get_bloginfo( 'title' ) ?></strong> | <?php printf( esc_html__( 'Made&nbsp;in&nbsp;%1$s', 'cssdrive' ), '<a class="uk-link-reset" href="//thefubon.com" target="_blank"><strong>FUBON</strong></a>' ); ?>
+		  &copy&nbsp;1945-<?php echo date( 'Y' ) ?> <strong><?php echo get_bloginfo( 'title' ) ?></strong> | <?php printf( esc_html__( 'Made&nbsp;in&nbsp;%1$s', 'cssdrive' ), '<a class="uk-link-reset" href="//thefubon.com" target="_blank"><strong>FUBON</strong></a>' ); ?>
 		  
 		</div>
 	</div>
